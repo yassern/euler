ca8e6f709c5c6b43dbae5224d3b59d09516cde3c
yasser nascimento <yassern@protonmail.com>
2017-09-22 13:29:49 -0300

    problem-4 resolvido

---
+++ problem-4.py
@@ -0,0 +1,16 @@
+def is_palindrome(string):
+    if len(string) < 2:
+        return True
+    if string[0] == string[-1]:
+        return is_palindrome(string[1:-1])
+    else:
+        return False
+
+largest_palindrome = 0
+for i in range(999, 99, -1):
+    for j in range(999, i - 1, -1):
+        mult = i * j
+        if is_palindrome(str(mult)):
+            if mult > largest_palindrome:
+                largest_palindrome = mult
+print(largest_palindrome)
