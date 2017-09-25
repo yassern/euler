f119a24db27e6f4df228ef2471e2e1f22c5f4ea9
yasser nascimento <yassern@protonmail.com>
2017-09-25 18:33:36 -0300

    problem-6 resolvido

---
+++ problem-6.py
@@ -0,0 +1,8 @@
+from functools import reduce
+
+interval = list(range(1, 101))
+
+sum_of_the_squares = reduce(lambda _sum, x: _sum + (x ** 2), interval)
+square_of_the_sum = sum(interval)**2
+
+print(square_of_the_sum - sum_of_the_squares)
